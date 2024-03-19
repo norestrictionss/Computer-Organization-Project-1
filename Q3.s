2bb66e5fa6e820a633f02fa8b88ca72b55c09d84
barisgirayakman <girayakman@gmail.com>
2024-03-19 14:02:28 +0300

    initial commit

---
+++ Q3.s
@@ -0,0 +1,32 @@
+
+.text
+.globl main
+
+
+main:
+
+    
+	
+	
+	
+	modify2:
+		beq $s0, $zero, create_substring
+		
+		move $s1, $s2, $s3
+		srl  $s1, $s1, 1
+		
+		j 
+		
+	
+	
+	
+	create_substring:
+		
+
+.data
+    input: .space 81
+	
+	
+	
+	
+
