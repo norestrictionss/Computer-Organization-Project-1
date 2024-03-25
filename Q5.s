0ab45f1a215c698b83b73a61162623116c7eb3ad
barisgirayakman <girayakman@gmail.com>
2024-03-25 23:05:41 +0300

    q5 completely done

--- Q5.s
+++ Q5.s
@@ -8,6 +8,7 @@ main:
 	lb $s1, matrix($t0) 	# columns
 	li $s2, 2 # beginning
 	mul $s3, $s0, $s1 # element count
+	addi $s3, $s3, 2
 	li $s5, 0 # temp val
 	li $s6, 0 # island width
 	
@@ -22,23 +23,25 @@ main:
 		bne $t1, $t0, if_not_one
 		addi $s6, $s5, 1
 		if_not_one:
-			beq $s2, $s3, exit
 			addi $s2, $s2, 1
+			beq $s2, $s3, exit
 			j for_loop
 	exit:
 		li $v0, 10
 		syscall
 	dfs:
-		addi $sp, $sp, -8
+		addi $sp, $sp, -16
 		sw $ra, 0($sp)
 		sb $a0, 4($sp) # beginning
 		sb $zero, matrix($a0)
 		addi $t7, $a0, -2
 		div $t7, $s1
 
-		
 		mflo $t2 # i
 		mfhi $t4 # j
+		
+		sb $t2, 8($sp)
+		sb $t4, 12($sp)
 		
 		first_if:
 			addi $t6, $s0, -1 # grid.length-1
@@ -50,23 +53,29 @@ main:
 			add $a0, $a0, $s1  # i += 1
 			addi $a1, $a1, 1 # result+1
 			jal dfs
+			addi $sp, $sp, 16
 			lb $a0, 4($sp)
 			lw $ra, 0($sp)
-			addi $sp, $sp, 8
+			lb $t2, 8($sp)
+			lb $t4, 12($sp)
+			
 		second_if:
 				addi $t6, $s1, -1 # grid[0].length-1
 				slt $t3, $t4, $t6 # j<grid[0].length-1
 				beq $t3, $zero, third_if
 				addi $t5, $a0, 1 
+
 				lb $t5, matrix($t5) # grid[i][j+1]
 				beq $t5, $zero, third_if
 				addi $a0, $a0, 1 # j+=1
 				addi $a1, $a1, 1 # result + 1
 				jal dfs
+				addi $sp, $sp, 16
 				lb $a0, 4($sp)
 				lw $ra, 0($sp)
-				addi $sp, $sp, 8
-			third_if:
+				lb $t2, 8($sp)
+				lb $t4, 12($sp)
+		third_if:
 					slt $t3, $t4, $t0 # j<1
 					beq $t3, $t0, fourth_if
 					addi $t5, $a0, -1
@@ -75,9 +84,11 @@ main:
 					addi $a0, $a0, -1 #j-=1
 					addi $a1, $a1, 1 # result+1
 					jal dfs
+					addi $sp, $sp, 16
 					lb $a0, 4($sp)
 					lw $ra, 0($sp)
-					addi $sp, $sp, 8
+					lb $t2, 8($sp)
+					lb $t4, 12($sp)
 				fourth_if:
 						slt $t3, $t2, $t0 # i<1
 						beq $t3, $t0, end_of_recursive
@@ -87,17 +98,20 @@ main:
 						sub $a0, $a0, $s1 # i-1
 						addi $a1, $a1, 1
 						jal dfs
+						addi $sp, $sp, 16
 						lb $a0, 4($sp)
 						lw $ra, 0($sp)
-						addi $sp, $sp, 8
+						lb $t2, 8($sp)
+						lb $t4, 12($sp)
 						end_of_recursive:
 							move $v1, $a1
 							jr $ra
 			
+			
 		
 
 .data
-   matrix: .byte 5, 6, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0
+   matrix: .byte 5, 6, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0
 	
 	
 	
